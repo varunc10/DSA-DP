21808be3e7d4b7d6d04e47235af16b9e0c3df307
Varun Chanda <99337212+varunc10@users.noreply.github.com>
2022-11-24 13:48:41 +0530

    Time: 435 ms (34.07%), Space: 9 MB (78.94%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> v(n+1,INT_MAX);
+        v[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                v[i]=min(v[i],v[i-(j*j)]+1);
+            }
+        }
+        return v[n];
+    }
+};
